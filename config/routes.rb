f68471d644635ee467ab78cc672ea88759817620
PsijicSeht <38445096+Kumar-Krishnan@users.noreply.github.com>
2018-07-25 11:39:14 -0400

    rolled back migrations, dropped db, re applied migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do
-    resources :users do
-      resources :snaps do
-        resources :beck_depressions
-        resources :social_anxiety_interactions
-      end
-    end
-  end
+  
 end

a7f47b99ef46059125b54386656973d2fb2a1479
PsijicSeht <38445096+Kumar-Krishnan@users.noreply.github.com>
2018-07-24 20:09:36 -0400

    functional user create route

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,4 +3,32 @@ class Api::UsersController < ApplicationController
         @users = User.all
         render json: @users
     end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def create
+        @user = User.create(user_params)
+        render json: @user
+    end
+
+    # def update
+    #     @city = City.find(params[:id])
+    #     @city.update!(city_params)
+    
+    #     render json: @city
+    #   end
+    
+    #   def destroy
+    #     @city = City.find(params[:id]).delete
+    
+    #     render status: :ok
+    #   end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :age, :gender)
+    end
 end

02eb26b7ca24fecea9c5fd39733afe451ee4f0a8
PsijicSeht <38445096+Kumar-Krishnan@users.noreply.github.com>
2018-07-25 19:02:41 -0400

    functional test pull route

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,2 +1,33 @@
 class Api::QuestionsController < ApplicationController
+    def index
+        @questions = Test.find(params[:test_id]).questions
+        render json: @questions
+    end
+
+    # def show
+    #     @test = Test.find(params[:id])
+    #     render json: @test
+    # end
+
+    # def create
+    #     @snap = Snap.find(params[:snap_id])
+    #     @test = @snap.tests.create(test_params)
+    #     render json: @test
+    # end
+
+    # # def update
+    # #     @post = Post.find(params[:id])
+    # #     @post.update(post_params)
+    # #     render json: @post
+    # # end
+
+    # def destroy
+    #     @test = Test.find(params[:id]).destroy
+    #     render status: :ok
+    # end
+
+    # private
+    # def test_params
+    #     params.require(:test).permit(:name, :template, :score, :measure)
+    # end
 end
